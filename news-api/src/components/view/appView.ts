45704a8c2ab8bfc97519a8819891d6a23dac0a47
zhadan93 <zhadan.evgenya@yandex.ru>
2022-07-05 16:16:42 +0300

    refactor: change typing of AppView class members

--- news-api/src/components/view/appView.ts
+++ news-api/src/components/view/appView.ts
@@ -1,18 +1,22 @@
 import News from './news/news';
 import Sources from './sources/sources';
+import { SourceList, NewsList } from '../app/interfaces';
 
 export class AppView {
+    private news: News;
+    private sources: Sources;
+
     constructor() {
         this.news = new News();
         this.sources = new Sources();
     }
 
-    drawNews(data) {
+    drawNews(data?: NewsList): void {
         const values = data?.articles ? data?.articles : [];
         this.news.draw(values);
     }
 
-    drawSources(data) {
+    drawSources(data?: SourceList): void {
         const values = data?.sources ? data?.sources : [];
         this.sources.draw(values);
     }

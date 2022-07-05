c1f6fb070744144437f7614b76c769c020909f3c
zhadan93 <zhadan.evgenya@yandex.ru>
2022-07-05 10:08:27 +0300

    feat: add data interfaces

---
+++ news-api/src/components/app/interfaces.ts
@@ -0,0 +1,30 @@
+export interface NewsDetails {
+    source: Pick<SourceDetails, 'id' | 'name'>;
+    author: string | null;
+    title: string;
+    description: string;
+    url: string;
+    publishedAt: string;
+    urlToImage: string;
+}
+
+export interface NewsList {
+    status: string;
+    totalResults: number;
+    articles: NewsDetails[];
+}
+
+export interface SourceDetails {
+    id: string;
+    name: string;
+    description: string;
+    url: string;
+    category: string;
+    country: string;
+    language: string;
+}
+
+export interface SourceList {
+    status: string;
+    sources: SourceDetails[];
+}

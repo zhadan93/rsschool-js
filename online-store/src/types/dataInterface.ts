53b8123041a230851a870c68d19d861e0f7a0334
zhadan93 <zhadan.evgenya@yandex.ru>
2022-07-14 01:26:31 +0300

    feat: add data and its interface

---
+++ online-store/src/types/dataInterface.ts
@@ -0,0 +1,10 @@
+export default interface CardDetails {
+  id: string,
+  name: string,
+  quantity: string,
+  year: string,
+  producer: string,
+  color: string,
+  material: string,
+  favorite: boolean,
+}

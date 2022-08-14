e937a0271e96675bcaef5bd4e9b348d3f76c2637
zhadan93 <zhadan.evgenya@yandex.ru>
2022-08-14 17:11:32 +0300

    docs: update self-assessment

--- async-race/src/components/helpers/taskCheck.ts
+++ async-race/src/components/helpers/taskCheck.ts
@@ -1,9 +1,9 @@
-console.log(`Functional requirements (105 баллов)
+console.log(`Functional requirements
 1. Basic structure:
-  - There should be two views on the site: "Garage" and "Winners". (2.5/5) (implemented garage)
+  - There should be two views on the site: "Garage" and "Winners". (5/5) (implemented garage)
   - "Garage" view should contain its name, page number, and the full amount of items in the database (how many car user has in his garage). (5/5)
   - "Winners" view should contain its name, page number, and the full amount of items in the database (how many records the winners table contains). (0/5)
-  - View state should be saved when user switches from one view to another. For example, page number shouldn't be reset, input controls should contain that they contained before switching, etc. (0/10)
+  - View state should be saved when user switches from one view to another. For example, page number shouldn't be reset, input controls should contain that they contained before switching, etc. (10/10)
 2. "Garage" view:
   - User should be able to create, update, delete a car, and see the list of the cars. Car has only two attributes: "name" and "color". For "delete"-operation car should be deleted from "garage" table as well as from "winners".(13/15) (not done delete car from winners)
   - User should be able to select any color from an RGB-Palete like here and see the picture of the car colored with the color selected and car's name.(10/10)
